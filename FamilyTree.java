7510ba852ff3ab1b78be97509a24af79c5971ae6
ASVolos <146623139+ASVolos@users.noreply.github.com>
2024-07-22 20:45:47 +0500

    Update FamilyTree.java

--- FamilyTree.java
+++ FamilyTree.java
@@ -5,23 +5,9 @@ import FamilyTree.Human.Human;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class FamilyTree implements Serializable {
     private List<Human> familyMembers;
-    Scanner sc = new Scanner(System.in);
-
-    public void serializeFamilyMembers() throws IOException {
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("family_members.ser"))) {
-            out.writeObject(familyMembers);
-        }
-    }
-
-    public void deserializeFamilyMembers() throws IOException, ClassNotFoundException {
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("family_members.ser"))) {
-            familyMembers = (List<Human>) in.readObject();
-        }
-    }
 
     public FamilyTree() {
         familyMembers = new ArrayList<>();
@@ -50,21 +36,5 @@ public class FamilyTree implements Serializable {
             stringBuilder.append("\n");
         }
         return stringBuilder.toString();
-    }
-
-
-    public static void seachPersonByName(Scanner sc, FamilyTree familyTree) {
-        System.out.println("Введите имя человека, которого нужно найти: ");
-        String name = sc.nextLine();
-        boolean found = false;
-        for (Human person : familyTree.getFamilyMember(name)) {
-            if (person.getName().equals(name) || person.getSecondName().equals(name)) {
-                System.out.println(person + System.lineSeparator());
-                found = true;
-            }
-        }
-        if (!found) {
-            System.out.println("Человек с таким именем не найден.");
-        }
     }
 }
